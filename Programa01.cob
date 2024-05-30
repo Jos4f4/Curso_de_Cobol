1c55a64d8c01ef2eac8db8918dc3164207169536
Josaf4 <josafa.soares2909@gmail.com>
2024-05-30 17:49:53 -0400

    101
    1010

---
+++ Programa01.cob
@@ -0,0 +1,23 @@
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA01.
+      *===========================================
+      *== AUTOR: JOSAFA; EMPRESA: XPTO
+      *==OBJETIVO: 1º CONTATO COM COBOL
+      *==DATA XX/XX/2024
+      *==OBSERVACOES:
+      *===========================================
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            DISPLAY "Hello World 2"
+            STOP RUN.
+       END PROGRAM PROGRAMA01.
